5978b17b83983179dd459d78fdfc3c84e09368bc
Soura Mandal <souramandal@pop-os.localdomain>
2022-02-24 15:04:34 +1100

    pushed post tut02 code

--- tut02/list/RList.c
+++ tut02/list/RList.c
@@ -1,17 +1,41 @@
 #include "List.h"
 
 int listSum(List l) {
-    return 0;
+    if (l == NULL) {
+        return 0;
+    } else {
+        return l->value + listSum(l->next);
+    }
 }
 
 int listCountOdds(List l) {
-    return 0;
+    if (l == NULL) return 0;
+
+    if (l->value % 2 == 1) {
+        return 1 + listCountOdds(l->next);
+    } else {
+        return 0 + listCountOdds(l->next);
+    }
 }
 
 bool listIsSorted(List l) {
-    return false;
+    if (l == NULL) return true;
+    if (l->next == NULL) return true;
+    if (l->value > l->next->value) return false;
+
+    return listIsSorted(l->next);
 }
 
 List listDelete(List l, int value) {
-   return NULL; 
+    if (l == NULL) return NULL;
+
+    if (l->value == value) {
+        List rest_of_list = l->next;
+        free(l);
+        return rest_of_list;
+    } else {
+        List rest_of_list = l->next;
+        l->next = listDelete(rest_of_list, value);
+        return l;
+    }
 }
--- tut02/vowels.h
+++ tut02/vowels.h
@@ -1,6 +1,8 @@
 #ifndef VOWELS_H
 #define VOWELS_H
 
+#include <string.h>
+
 int count_vowels(char *str);
 
 #endif
